7b0798f68bab43c9c2fe98c6a344b7a99e903492
magic-onion <alexander.m.truong@gmail.com>
2018-10-16 14:45:30 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ('Mr. Chocolate', 10, 'M', 'grey', 'Friendly', 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ('Rowdy', 10, 'M', 'dark brown', 'Excited', 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ('Tabitha', 7, 'F', 'dark brown', 'Calm', 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ('Seargeant Brown', 4, 'F', 'grey', 'Calm', 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ('Melissa', 6, 'F', 'choc', 'Defensive', 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ('Grinch', 2, 'M', 'dark brown', 'Grumpy', 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ('Wendy', 8, 'F', 'Black', 'Angry', 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES (NULL, 6, 'M', 'Brown', 'Aggressive', 1);
